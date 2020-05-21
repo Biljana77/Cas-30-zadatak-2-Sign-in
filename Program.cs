7e2e34382843fbfd1a7305b72b698e6add5c45b7
Biljana Dutina <biljana.dutina@yahoo.com>
2020-05-21 19:10:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cas_30_zadatak_2_Sign_in
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Selenium tests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;
+using EC = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+
+namespace Cas_30_zadatak_2_Sign_in
+{
+    class Selenium_tests
+    {
+        IWebDriver driver;
+        WebDriverWait wait;
+
+        public void Navigate(string Url)
+        {
+            driver.Navigate().GoToUrl(Url);
+        }
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+
+        }
+        [Test]
+        public void TestLogin()
+        {
+            Navigate("http://shop.qa.rs");
+            IWebElement LinkLogIn = driver.FindElement(By.XPath("//a[@href='/login']"));
+            if (LinkLogIn.Displayed && LinkLogIn.Enabled)
+            {
+                LinkLogIn.Click();
+            }
+            IWebElement userName = wait.Until(EC.ElementIsVisible(By.Name("username")));
+            if (userName.Displayed && userName.Enabled)
+            {
+                userName.SendKeys("Pera");
+            }
+            IWebElement inputLozinka = driver.FindElement(By.Name("password"));
+            if (inputLozinka.Displayed && inputLozinka.Enabled)
+            {
+                inputLozinka.SendKeys("Peric");
+            }
+            IWebElement buttonLogin = driver.FindElement(By.Name("password"));
+            if (buttonLogin.Displayed && buttonLogin.Enabled)
+            {
+                buttonLogin.Click();
+            }
+
+
+        }
+    }
+}
+
